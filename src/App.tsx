46f979ea3175626a3d7f4914ee62d4c2b166301b
Daybreak312 <ty82afg12@gmail.com>
2024-10-06 00:22:40 +0900

    w

--- src/App.tsx
+++ src/App.tsx
@@ -16,25 +16,25 @@ export const App = () => {
 
     return (
         <main
-            className={"flex flex-col justify-center gap-[64px] px-[128px] rounded-[64px] bg-[#222222]"}>
+            className={"flex flex-col justify-center gap-[64px] rounded-[64px] bg-[#222222]"}>
             <div className={"flex flex-col gap-[20px]"}>
-                <p className={"font-p-regular text-[40px] text-white text-center whitespace-normal break-keep text-pretty"}>
+                <p className={"font-p-regular text-[24px] text-white text-center whitespace-normal break-keep text-pretty"}>
                     "{quote[0]}"
                 </p>
-                <p className={"font-p-medium text-[28px] text-white text-right"}>
+                <p className={"font-p-medium text-[20px] text-white text-right"}>
                     - {quote[1]}
                 </p>
             </div>
             <div className={"flex flex-col gap-[10px]"}>
-                <p className={"font-p-medium text-[32px] text-white text-center"}>
+                <p className={"font-p-medium text-[24px] text-white text-center"}>
                     {getCurrentDateWithFormat()}
                 </p>
-                <p className={"font-p-medium text-[32px] text-white text-center"}>
+                <p className={"font-p-medium text-[24px] text-white text-center"}>
                     <span className={"text-[#FF7F11]"}>{cal.percentage}</span>% 진행됨, <span
                     className={"text-[#FF7F11]"}>{cal.daysRemaining}</span>일 남음.
                 </p>
                 <div
-                    className={"flex justify-center items-center p-[10px] h-[102px] w-[1024px] bg-[#333333] rounded-[100px]"}>
+                    className={"flex justify-center items-center p-[10px] h-[80px] w-[500px] bg-[#333333] rounded-[100px]"}>
                     <div
                         className={"flex justify-start items-center p-[10px] h-full w-full bg-[#222222] rounded-[100px]"}>
                         <div className={"flex justify-start items-center h-full w-full rounded-[100px]"}>
