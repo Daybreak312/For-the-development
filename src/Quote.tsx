8b73b30daaa640e3aadeb2c4e26ecd8203ddfdfa
Daybreak312 <ty82afg12@gmail.com>
2024-10-07 20:17:00 +0900

    UPDATE :: 타이머 방식 수정

--- src/Quote.tsx
+++ src/Quote.tsx
@@ -1,39 +1,52 @@
 import {useEffect, useState} from "react";
 import {getRandomQuote} from "./RandomQuote.tsx";
 
-let intervalId: number = 0;
+let lastRefreshedMinute: number;
+let lastRefreshedSecond: number;
 
 export const Quote = () => {
 
     const [quote, setQuote] = useState(getRandomQuote);
 
-    function setQuoteRandom() {
-        setQuote(getRandomQuote);
+    useEffect(() => {
+        resetLastRefreshedTime();
+
+        setInterval(refreshQuoteIfSetTimePassed, 200);
+        document.addEventListener("keydown", keyDownEvent);
+        document.addEventListener("keyup", keyUpEvent);
+    }, []);
+
+    function refreshQuoteIfSetTimePassed() {
+        const current = new Date();
+
+        // Refresh time
+        if (current.getMinutes() != lastRefreshedMinute && current.getSeconds() == lastRefreshedSecond) {
+            setQuoteRandom();
+            resetLastRefreshedTime(1);
+        }
     }
 
     function keyDownEvent(e: KeyboardEvent) {
         document.removeEventListener("keydown", keyDownEvent)
-        clearInterval(intervalId - 1);
-        clearInterval(intervalId);
         e.stopPropagation();
+        lastRefreshedSecond = 61; // Disable refresh
         setQuoteRandom();
-
-        console.log(intervalId)
     }
 
-    function addKeyPressEventListener() {
+    function keyUpEvent() {
         document.addEventListener("keydown", keyDownEvent);
-        intervalId = setInterval(setQuoteRandom, 60000);
-
-        console.log(intervalId)
+        resetLastRefreshedTime();
     }
 
-    useEffect(() => {
-        addKeyPressEventListener();
-        document.addEventListener("keyup", addKeyPressEventListener);
+    function setQuoteRandom() {
+        setQuote(getRandomQuote);
+    }
 
-        console.log(intervalId)
-    }, []);
+    function resetLastRefreshedTime(plusSecond: number = 0) {
+        const current = new Date();
+        lastRefreshedMinute = current.getMinutes();
+        lastRefreshedSecond = current.getSeconds() + plusSecond % 60;
+    }
 
     return <div className={"flex flex-col gap-[20px]"}>
         <p className={"font-p-medium text-[24px] text-white text-center whitespace-normal break-keep text-pretty"}>
